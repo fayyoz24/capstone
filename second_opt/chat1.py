6d7669f235eec9d00af1eec43c2d1f7ef86e1900
Fayyoz24 <fayyozusmon@gmail.com>
2022-08-26 09:27:17 +0200

    gjhk

---
+++ second_opt/chat1.py
@@ -0,0 +1,70 @@
+from email.mime import audio
+import random
+import json
+import torch
+import speech_recognition as sr
+import pyaudio
+
+
+from model1 import NeuralNet
+from nltk_utils import bag_of_words, tokenize
+
+r = sr.Recognizer()
+
+def record_audio():
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        voice_data = ""
+        try:
+            voice_data = r.recognize_google(audio)
+            print(voice_data)
+        except sr.UnknownValueError:
+            print('Sorry, I did not get that')
+print('start speaking')
+print(record_audio())
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# with open('intents.json', 'r') as json_data:
+#     intents = json.load(json_data)
+
+# FILE = "data.pth"
+# data = torch.load(FILE)
+
+# input_size = data["input_size"]
+# hidden_size = data["hidden_size"]
+# output_size = data["output_size"]
+# all_words = data['all_words']
+# tags = data['tags']
+# model_state = data["model_state"]
+
+# model = NeuralNet(input_size, hidden_size, output_size).to(device)
+# model.load_state_dict(model_state)
+# model.eval()
+
+# bot_name = "Sam"
+# print("Let's chat! (type 'quit' to exit)")
+# while True:
+#     # sentence = "do you use credit cards?"
+#     sentence = input("You: ")
+#     if sentence == "quit":
+#         break
+
+#     sentence = tokenize(sentence)
+#     X = bag_of_words(sentence, all_words)
+#     X = X.reshape(1, X.shape[0])
+#     X = torch.from_numpy(X).to(device)
+
+#     output = model(X)
+#     _, predicted = torch.max(output, dim=1)
+
+#     tag = tags[predicted.item()]
+
+#     probs = torch.softmax(output, dim=1)
+#     prob = probs[0][predicted.item()]
+#     if prob.item() > 0.75:
+#         for intent in intents['intents']:
+#             if tag == intent["tag"]:
+#                 print(f"{bot_name}: {random.choice(intent['responses'])}")
+#     else:
+#         print(f"{bot_name}: I do not understand...")
+
